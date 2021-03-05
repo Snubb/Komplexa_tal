a7eb800a9cad35c194a1c3e11be5386ebc702fd0
Snubb <60389050+Snubb@users.noreply.github.com>
2021-03-05 10:32:19 +0100

    Skrev lite kommentarer(även fast det inte känns som det behövs)

--- src/Komplex.java
+++ src/Komplex.java
@@ -9,31 +9,31 @@ public class Komplex {
         this.imaginar = imaginar;
     }
 
-    public Komplex add(Komplex b) {
+    public Komplex add(Komplex b) { //adderar med formeln: (a+bi) + (c+di) = (a+c) + (b+d)i
         double realTemp = this.real + b.real;
         double imaginarTemp = this.imaginar + b.imaginar;
 
         return new Komplex(realTemp, imaginarTemp);
     }
 
-    public Komplex multiply(Komplex b) {
+    public Komplex multiply(Komplex b) { //multplicerar med formeln: (a+bi)(c+di) = (ac-bd) + (bc+ad)i
         double realTemp = (this.real * b.real) - (this.imaginar * b.imaginar);
         double imaginarTemp = (this.imaginar * b.real) + (this.real * b.imaginar);
 
         return new Komplex(realTemp, imaginarTemp);
     }
 
-    public double arg() {
+    public double arg() { //tar ut argumentet enligt formeln: taninvers(b/a)
 
         return Math.atan(this.imaginar/this.real);
     }
 
-    @Override
+    @Override //fixar så att toString skriver en läsbar string istället för gibberish
     public String toString() {
         return real + " + " + imaginar + "i";
     }
 
-    @Override
+    @Override //gör så att equals fungerar
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
